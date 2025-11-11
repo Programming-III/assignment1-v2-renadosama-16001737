8aa65b2d8dd3b6effeca807d7f501d6a06ce3147
renadosama-16001737 <renadosama278@gmail.com>
2025-11-11 09:34:35 +0200

    Update Course.h

--- Course.h
+++ Course.h
@@ -16,18 +16,18 @@ int currentStudents;
 
 public:
 Course();
-Course( string courseCode, string courseName, int maxStudents, Student* students, int currentStudents);
+Course(string courseCode, string courseName, int maxStudents, Student* students, int currentStudents);
 ~Course();
 void addStudent(const Student& s);
 displayCourseInfo();
-string getcourseCode();
-string getcourseName();
-int getmaxStudents();
-int getcurrentStudents();
-void setcourseCode();
-void setcourseName();
-void setmaxStudents();
-void setcurrentStudent();
+string getCourseCode();
+string getCourseName();
+int getMaxStudents();
+int getCurrentStudents();
+void setCourseCode();
+void setCourseName();
+void setMaxStudents();
+void setCurrentStudent();
 
 
 
